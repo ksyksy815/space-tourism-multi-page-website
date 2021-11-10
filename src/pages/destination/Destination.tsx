1050556c01a8e752097d8af15eeac30c79eb2060
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-11-10 22:31:11 +0900

    [Update] Destination info's types

--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
+import DestinationInfo from './DestinationInfo'
+import DestiNav from './DestiNav'
+
 
 export default function Destination() {
+  const [current, setCurrent] = useState("Moon")
+
+
   return (
     <div>
-      
+      <div>
+        <span>01</span>
+        <span>PICK YOUR DESTINATION</span>
+      </div>
+      <div>
+        <DestiNav />
+        <DestinationInfo current={current}/>
+      </div>
     </div>
   )
 }
--- src/pages/destination/DestinationInfo.tsx
+++ src/pages/destination/DestinationInfo.tsx
@@ -1,26 +1,34 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { fetchDestiInfo, DestinationInterface } from './fetchDestiInfo'
 
-export default function DestinationInfo( {current: string} ) {
-  useEffect(()=> {
+interface DestiInfoProps {
+  current: string
+}
 
-  })
+export default function DestinationInfo( { current }: DestiInfoProps ) {
+  const [info, setInfo] = useState<DestinationInterface>({} as DestinationInterface)
+
+  useEffect (()=> {
+    const fetchedInfo = fetchDestiInfo(current)
+    setInfo(fetchedInfo)
+  }, [current])
 
   return (
     <div>
       <img src="" alt=""/>
       <section>
         <div>
-          <h1>{name}</h1>
-          <p>{description}</p>
+          <h1>{info.name}</h1>
+          <p>{info.description}</p>
         </div>
         <div>
           <div>
             <span>AVG. DISTANCE</span>
-            <span>{distance}</span>
+            <span>{info.distance}</span>
           </div>
           <div>
             <span>EST. TRAVEL TIME</span>
-            <span>{travel}</span>
+            <span>{info.travel}</span>
           </div>
         </div>
       </section>
