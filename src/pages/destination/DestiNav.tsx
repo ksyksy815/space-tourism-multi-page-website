fa92571909acc0c56e2f8a4189cfe70a0ac4e449
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-11-12 22:52:37 +0900

    [Update] nav onclick event with types

--- src/pages/destination/DestiNav.tsx
+++ src/pages/destination/DestiNav.tsx
@@ -5,13 +5,23 @@ const StyledDestiNav = styled.ul`
   display: flex;
 `
 
-export default function DestiNav( {} ) {
+interface DestiNavProps {
+  handleCurrent(str: string | null): void
+}
+
+export default function DestiNav ( {handleCurrent}: DestiNavProps ) {
+  const options = ["Moon", "Mars", "Europa", "Titan"]
+
+  const handleClick = (e: React.MouseEvent<HTMLLIElement>): void => {
+    const li = e.target as HTMLElement
+    handleCurrent(li.textContent)
+  }
+
   return (
     <StyledDestiNav>
-      <li>MOON</li>
-      <li>MARS</li>
-      <li>EUROPA</li>
-      <li>TITAN</li>
+      {
+        options.map((item, i) => <li onClick={handleClick} key={i}>{item}</li>)
+      }
     </StyledDestiNav>
   )
 }
--- src/pages/destination/Destination.tsx
+++ src/pages/destination/Destination.tsx
@@ -1,28 +1,76 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import DestinationInfo from './DestinationInfo'
 import DestiNav from './DestiNav'
 import styled from 'styled-components';
 import { Page } from '../../styles/commonStyles'
 import bgImageDesktop from '../../assets/destination/background-destination-desktop.jpg'
+import moon from "../../assets/destination/image-moon.png"
+import mars from "../../assets/destination/image-mars.png"
+import europa from "../../assets/destination/image-europa.png"
+import titan from "../../assets/destination/image-titan.png"
 
 const DestinationPage = styled(Page)`
   background: url(${bgImageDesktop});
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 3rem;
+  row-gap: 2rem;
+`
+
+const DestiTitle = styled.div`
+  font-size: 2rem;
+  display: flex;
+  column-gap: 2rem;
+`
+
+const DestiInfoBox = styled.section`
+  border: 1px solid white;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 `
 
 export default function Destination() {
-  const [current, setCurrent] = useState("Moon")
+  const [current, setCurrent] = useState<string | null>("Moon")
+  const [imageURL, setImageURL] = useState(moon)
 
+  const handleCurrent = (str: string | null) => {
+    setCurrent(str)
+  }
+  
+  useEffect(() => {
+    switch (current) {
+      case "Moon":
+        setImageURL(moon)
+        break;
+      case "Mars":
+        setImageURL(mars)
+        break;
+      case "Europa":
+        setImageURL(europa)
+        break;
+      case "Titan":
+        setImageURL(titan)
+        break;
+      default: return;
+    }
+  }, [current])
 
   return (
     <DestinationPage>
-      <div>
+      <DestiTitle>
         <span>01</span>
         <span>PICK YOUR DESTINATION</span>
-      </div>
-      <div>
-        <DestiNav />
-        <DestinationInfo current={current}/>
-      </div>
+      </DestiTitle>
+      <DestiInfoBox>
+        <img src={imageURL} alt={current}></img>
+        <div>
+          <DestiNav handleCurrent={handleCurrent}/>
+          <DestinationInfo current={current}/>
+        </div>
+      </DestiInfoBox>
     </DestinationPage>
   )
 }
